f2c9602ca434731e3726e399776a315ffa2d5a75
taehong9301 <public.devhong@gmail.com>
2021-07-27 23:09:24 +0900

    header component 생성 (#2)

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+import LogoImage from "./LogoImage";
+
+const Header = (props) => {
+  return (
+    <div>
+      <LogoImage />
+    </div>
+  );
+};
+
+Header.propTypes = {};
+
+export default Header;
