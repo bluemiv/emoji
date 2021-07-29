5cf6f0faa4bb127f7952c38cb4d0a3330d7780b0
taehong9301 <public.devhong@gmail.com>
2021-07-29 12:54:39 +0900

    이모지 복사 기능 추가 (#7)

--- src/components/main/EmojiBox.jsx
+++ src/components/main/EmojiBox.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Tooltip } from "antd";
+import { onClickOkNotification } from "../common/Notification";
 
 const EmojiBox = ({ emojiInfo }) => {
   const { id, emoji, unicode, name } = emojiInfo;
+
+  const onClickHandler = () => {
+    navigator.clipboard.writeText(emoji);
+    onClickOkNotification(
+      "복사 완료",
+      `${emoji}를 원하시는곳에 붙여 사용하세요!`
+    );
+  };
+
   return (
-    <div className="emojibox__item">
+    <div className="emojibox__item" onClick={onClickHandler}>
       <Tooltip title={name}>
         <div className="emojibox__item-emoji">{emoji}</div>
       </Tooltip>
