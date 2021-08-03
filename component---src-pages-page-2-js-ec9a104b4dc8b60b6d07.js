a17f7b0415796be437b071565f098796283bd293
taehong9301 <public.devhong@gmail.com>
2021-08-04 00:12:05 +0900

    Updates

---
+++ component---src-pages-page-2-js-ec9a104b4dc8b60b6d07.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkemoji=self.webpackChunkemoji||[]).push([[617],{9863:function(e,t,n){n.r(t);var l=n(7294),a=n(5444),c=n(5127),o=n(3751);t.default=function(){return l.createElement(c.Z,null,l.createElement(o.Z,{title:"Page two"}),l.createElement("h1",null,"Hi from the second page"),l.createElement("p",null,"Welcome to page 2"),l.createElement(a.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-ec9a104b4dc8b60b6d07.js.map
